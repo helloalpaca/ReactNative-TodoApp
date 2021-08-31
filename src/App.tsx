68e4424b3019405c4ab9449d257d036b928d223c
helloalpaca <jms393497@gmail.com>
2021-08-31 23:47:44 +0900

    Add onboarding-swiper
    react-native-onboarding-swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
  import React, {Fragment, useRef, useEffect } from 'react';
- import type {Node} from 'react';
+ import Styled from 'styled-components/native';
  
  import {
    SafeAreaView,
@@ -18,6 +10,9 @@
    useColorScheme,
    View,
    Animated,
+   Button,
+   Alert,
+   Pressable,
  } from 'react-native';
  
  import {
@@ -27,39 +22,16 @@
    LearnMoreLinks,
    ReloadInstructions,
  } from 'react-native/Libraries/NewAppScreen';
- 
- import Styled from 'styled-components/native';
- import TypeWriter from 'react-native-typewriter';
- 
- const Section = ({children, title}): Node => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
 
  interface Props {}
  
  const App = ({}: Props) => {
+   const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+   useEffect(() => {
+     
+  });
+
    const isDarkMode = useColorScheme() === 'dark';
  
    const backgroundStyle = {
@@ -70,8 +42,11 @@
      <SafeAreaView>
        <Fragment>
          <View>
-           <Text style={styles.textHelloworld}>Hello, World!</Text>
-           <TextHelloWorldStyled>Hello World, Styled!</TextHelloWorldStyled>      
+           {/* <Button title="TITLE" onPress = {() => Alert.alert("Button Clicked!")} /> */}
+           <TextHelloWorldStyled>Hello World, Styled!</TextHelloWorldStyled>     
+           <Pressable style={styles.buttonStyle} onPress={()=> Alert.alert("Button Clicked!")}>
+            <Text style={styles.textHelloworld}>Hello!</Text>
+          </Pressable>
          </View>
        </Fragment>
      </SafeAreaView>
@@ -79,26 +54,17 @@
  };
  
  const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
    highlight: {
      fontWeight: '700',
    },
    textHelloworld: {
-     fontSize: 36,
+     fontSize: 24,
      color: Colors.dark,
    },
+   buttonStyle: {
+     backgroundColor: 'yellow',
+     borderWidth: 5,
+   }
  });
  
  const TextHelloWorldStyled = Styled.Text`
---
+++ src/Onboarding.tsx
@@ -0,0 +1,36 @@
+import { Image } from 'react-native';
+import React from 'react';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+interface Props {}
+
+const App = ({}: Props) => { 
+    return (
+        <Onboarding
+        onDone={() => console.log('done')}
+        pages={[
+          {
+            backgroundColor: '#fff',
+            image: <Image source={require('./images/circle.png')} />,
+            title: 'Onboarding',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          },
+          {
+            backgroundColor: '#fe6e58',
+            image: <Image source={require('./images/square.png')} />,
+            title: 'The Title',
+            subtitle: 'This is the subtitle that sumplements the title.',
+          },
+          {
+            backgroundColor: '#999',
+            image: <Image source={require('./images/triangle.png')} />,
+            title: 'Triangle',
+            subtitle: "Beautiful, isn't it?",
+          },
+        ]}
+      />
+    );
+};
+
+export default App;
