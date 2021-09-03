bbd4d0448b20c1dc61679afa828ed6d310188d43
helloalpaca <jms393497@gmail.com>
2021-09-04 01:38:21 +0900

    Implement Navigation

--- src/Screens/Main/index.tsx
+++ src/Screens/Main/index.tsx
@@ -23,51 +23,43 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-interface Props {}
+interface Props {
+   navigation: any;
+}
 
 const Main = ({ navigation }: Props) => {
-  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-
-  useEffect(() => {
-    
- });
-
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <SafeAreaView>
       <Fragment>
         <View>
-          {/* <Button title="TITLE" onPress = {() => Alert.alert("Button Clicked!")} /> */}
-          <TextTitleLines>누구나 손 쉬운 퀵배송</TextTitleLines>
-          <TextTitleLines>지금 시작하세요</TextTitleLines>
-          <ImgMain source={require('./images/circle.png')}></ImgMain>
+          <TextTitleLines1>누구나 손 쉬운 퀵배송</TextTitleLines1>
+          <TextTitleLines2>지금 시작하세요</TextTitleLines2>
+          <ImgView>
+            <ImgMain source={require('../../images/circle.png')}></ImgMain>
+          </ImgView>
+          <TextBOXN>BOX N</TextBOXN>
           <ButtonKakaoStart onPress={() => {
               navigation.navigate('Login', {});
             }}>
             <ButtonText>카카오톡으로 시작할래요</ButtonText>
           </ButtonKakaoStart>
-          <ButtonKakaoStart onPress={() => Alert.alert("Button Clicked!")}>
+          <ButtonKakaoStart onPress={() => {
+              navigation.navigate('SignUp', {});
+            }}>
             <ButtonText>휴대폰 번호로 시작할래요</ButtonText>
           </ButtonKakaoStart>
-          <TextAsk>시작이 어려우신가요?</TextAsk>
-          <ButtonAsk>
+          <ViewAsk>
+          <TextAsk>시작이 어려우신가요? </TextAsk>
+          <ButtonAsk onPress={() => Alert.alert("Button Clicked!")}>
             <Text>바로 문의하기</Text>
           </ButtonAsk>
-          
-          {/* <TextHelloWorldStyled>Hello World, Styled!</TextHelloWorldStyled>     
-          <Pressable style={styles.buttonStyle} onPress={()=> Alert.alert("Button Clicked!")}>
-           <Text style={styles.textHelloworld}>Hello!</Text>
-         </Pressable> */}
+          </ViewAsk>
         </View>
       </Fragment>
     </SafeAreaView>
+    
   );
-
 };
 
 
@@ -91,37 +83,48 @@ const TextHelloWorldStyled = Styled.Text`
   color: ${Colors.dark};
 `;
 
-const TextTitleLines = Styled.Text`
- flex-direction: column;
- justify-content: center;
+const TextTitleLines1 = Styled.Text`
+ align-items: center;
+ padding: 0px 24px;
+ margin-top: 128px;
+ 
+ text-align: center;
+ font-style: normal;
+ font-weight: bold;
+ font-size: 20px;
+ line-height: 26px;
+`;
+
+const TextTitleLines2 = Styled.Text`
  align-items: center;
  padding: 0px 24px;
 
- height: 72px;
- left: 0px;
- right: 0px;
- top: 128px;
+ text-align: center;
+ font-style: normal;
+ font-weight: bold;
+ font-size: 32px;
+ line-height: 42px;
+`;
+
+const ImgView = Styled.View`
+ text-align: center;
+ margin-top: 38px;
 `;
 
 const ImgMain = Styled.Image`
+ align-self: center;
  width: 152px;
  height: 152px;
- left: 112px;
- top: 238px;
 `;
 
 const TextBOXN = Styled.Text`
-width: 194px;
-height: 84px;
-left: 92px;
-top: 378px;
-
-font-family: Noto Sans KR;
 font-style: normal;
 font-weight: bold;
 font-size: 48px;
 line-height: 70px;
 text-align: center;
+align-items: center;
+margin-bottom: 31px;
 
 color: #000000;
 `;
@@ -129,6 +132,7 @@ color: #000000;
 const ButtonKakaoStart = Styled.TouchableOpacity`
 align-items: center;
 padding: 12px 24px;
+margin: 14px 24px 0px 24px;
 
 background: #000000;
 border-radius: 8px;
@@ -142,6 +146,14 @@ line-height: 18px;
 color: #FFFFFF;
 `;
 
+const ViewAsk = Styled.View`
+width: 100%;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+margin-top: 36px;
+`;
+
 const TextAsk = Styled.Text`
 font-style: normal;
 font-weight: 500;
@@ -150,7 +162,6 @@ line-height: 21px;
 `;
 
 const ButtonAsk = Styled.TouchableOpacity`
-
 `;
 
 export default Main;
